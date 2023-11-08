eb7f2cf11f2851c7291cfb0e7b9a9d286fbe1e80
Kyle Baumgarten <BaumDev@proton.me>
2023-11-08 02:46:56 -0500

    Init

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 #run docker build -t mariadbfull .
 # run docker run -d --name my-mariadb-instance -p 3306:3306 mariadbfull
 FROM mariadb:latest
-ENV MYSQL_ROOT_PASSWORD=secret
+ARG PWD
+ENV MYSQL_ROOT_PASSWORD=$PWD
+ENV MYSQL_ROOT_HOST=%
 COPY ./sql-scripts/ /docker-entrypoint-initdb.d/
-
+EXPOSE 3306 
--- sql-scripts/initapi.sql
+++ sql-scripts/initapi.sql
@@ -1,2 +1,4 @@
 create database ApiKey;
-create table ApiKey.apikeys (INTERNAL_PRIMARY_KEY int primary key not null auto_increment, databaseuser varchar(100), apikey BIGINT);
+create table ApiKey.apikeys (INTERNAL_PRIMARY_KEY int primary key not null auto_increment, databaseuser varchar(100), databasepasshash varchar(100), apikey varchar(255));
+create database Relationships;
+create table Relationships.relationships (INTERNAL_PRIMARY_KEY int primary key not null auto_increment, targeted_database varchar(100), parent_table varchar(100), child_table varchar(100), where_clause varchar(255), relationship varchar(100) UNIQUE);
